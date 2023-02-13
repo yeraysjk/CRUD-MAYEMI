4ad874669b3c1adee67b4c5fc95dea3c45d120ee
marc_14 <retilla_20@hotmail.com>
2023-02-13 19:58:16 +0100

    formulari_dilluns

---
+++ forms/servei_afegeix.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+Description: <input type="text" name="description"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
