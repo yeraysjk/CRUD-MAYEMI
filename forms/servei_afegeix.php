b67d76d70ddaa64aa0f938ab08a0a9946d8b092d
marc_14 <retilla_20@hotmail.com>
2023-02-14 15:36:49 +0100

    marc2

--- forms/servei_afegeix.php
+++ forms/servei_afegeix.php
@@ -1,11 +1,12 @@
 <html>
 <head>
-  <title>Pregunta Nom i Descripció</title>
+  <title>Afegeix Nom i Descripció</title>
 </head>
 <body>
 
 <form action="servei_afegeix_id.php" method="post">
 Nom: <input type="text" name="name"><br>
+Preu: <input type="text" name="price"><br>
 Descripcció: <input type="text" name="description"><br>
 <input type="submit" value = "Enviar">
 </form>
