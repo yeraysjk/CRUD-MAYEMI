f791b24ac34c079ee31125ce603595a16434ddad
marc_14 <retilla_20@hotmail.com>
2023-02-14 15:03:48 +0100

    formulari resposta sense acabar Marc

--- forms/servei_afegeix.php
+++ forms/servei_afegeix.php
@@ -1,10 +1,13 @@
 <html>
+<head>
+  <title>Pregunta Nom i Descripció</title>
+</head>
 <body>
 
-<form action="welcome.php" method="post">
-Name: <input type="text" name="name"><br>
-Description: <input type="text" name="description"><br>
-<input type="submit">
+<form action="servei_afegeix_id.php" method="post">
+Nom: <input type="text" name="name"><br>
+Descripcció: <input type="text" name="description"><br>
+<input type="submit" value = "Enviar">
 </form>
 
 </body>
---
+++ forms/servei_afegeix_id.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Resposta act2</title>
+</head>
+<body>
+  Nom: <?php echo $_POST["name"]; ?><br><br>
+  Descripció: <?php echo $_POST["description"]; ?><br><br>
+
+<?php
+  $nom = $_POST["name"];
+  if ($nom == "") {
+    echo "<img src='\'>";
+  } else ()
+?>
+
+</body>
+</html>
+
