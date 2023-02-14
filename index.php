97e9979520d1ed503bc66564170ca6bbb4c7aa5d
Yeray <yeraysjkk@gmail.com>
2023-02-14 17:22:28 +0100

    Pruebas galeria_productes 14feb

--- index.php
+++ index.php
@@ -4,9 +4,10 @@ include('header.php');
 include('hero.php');
 include('about.php');
 include('services.php');
-include('portfolio.php');
+include('portfolio.php'); 
+include('galeria_productes.php'); /*-----pruebas*/
 include('team.php');
 include('counts.php');
-include('contact.php');
+include('contact.php'); 
 include('footer.php');
 ?>
