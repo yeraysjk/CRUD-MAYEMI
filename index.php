558ac016794a6d5cf64c55ecbb231b1a3e3f9f10
Mikita <nikita.kishkevich@gmail.com>
2023-02-17 16:53:45 +0100

    borrar galeriaproductes

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ include('hero.php');
 include('about.php');
 include('services.php');
 include('portfolio.php'); 
-include('galeria_productes.php'); /*-----pruebas*/
 include('team.php');
 include('counts.php');
 include('contact.php'); 
